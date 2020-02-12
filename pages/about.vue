6224575699a772d30b4bd3efc997003961e147ab
Jakub Voráček <jakub.voracek@gmail.com>
2020-02-12 14:33:15 +0100

    auth plugins proof of concept

---
+++ pages/about.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <amplify-connect :query="listTodosQuery">
+      <template slot-scope="{ loading, data, errors }">
+        <div v-if="loading">
+          Loading...
+        </div>
+        <div v-if="errors.length > 0">Errors</div>
+        <div v-else-if="data">
+          {{ data }}
+        </div>
+      </template>
+    </amplify-connect>
+  </div>
+</template>
+<script>
+import { components } from "aws-amplify-vue";
+const ListTodoQuery = `query {
+   listBlogs{
+    items{name}
+  }
+}
+  `;
+export default {
+  components: {
+    ...components
+  },
+  computed: {
+    listTodosQuery() {
+      return this.$Amplify.graphqlOperation(ListTodoQuery);
+    }
+  }
+};
+</script>
--- pages/home.vue
+++ pages/home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
-    <div v-if="!signedIn">kkk
+    <div v-if="!signedIn">
+      <amplify-authenticator v-bind:authConfig="authConfig"></amplify-authenticator>
       <amplify-authenticator />
     </div>
     <div v-else>
@@ -12,10 +13,36 @@
 <script>
 import { Auth } from "aws-amplify";
 import { AmplifyEventBus } from "aws-amplify-vue";
+
 export default {
+  name: "Home",
   data() {
     return {
-      signedIn: false
+      signedIn: false,
+      authConfig: {
+        usernameAttributes: "My user nameaaa",
+        signUpConfig: {
+          header: "My Customized Sign Up",
+          hideAllDefaults: true,
+          defaultCountryCode: "1",
+          signUpFields: [
+            {
+              label: "Uživatelské jméno",
+              key: "username",
+              required: true,
+              displayOrder: 1,
+              type: "string"
+            },
+            {
+              label: "Password",
+              key: "password",
+              required: true,
+              displayOrder: 2,
+              type: "password"
+            }
+          ]
+        }
+      }
     };
   },
   methods: {
@@ -23,15 +50,18 @@ export default {
       try {
         const user = await Auth.currentAuthenticatedUser();
         this.signedIn = true;
-        console.log(user);
+        console.log(user, "ahoj");
       } catch (err) {
         this.signedIn = false;
+        console.log("nic");
       }
     }
   },
   created() {
     this.findUser();
+    console.log(this.$Amplify);
     AmplifyEventBus.$on("authState", info => {
+      console.log("info", info);
       if (info === "signedIn") {
         this.findUser();
       } else {
