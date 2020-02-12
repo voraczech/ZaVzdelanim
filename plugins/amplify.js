fa570f53521761d94b4cd711ffe4b9cfcd2b8c85
Jakub Voráček <jakub.voracek@gmail.com>
2020-02-12 15:09:07 +0100

    get rid of unused plugins (POC successful)

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,12 +1,4 @@
-import Vue from 'vue'
-import Amplify, * as AmplifyModules from 'aws-amplify'
-import {
-  AmplifyPlugin,
-  components
-} from 'aws-amplify-vue'
+import Amplify from 'aws-amplify'
 import awsconfig from '../aws-exports'
 
 Amplify.configure(awsconfig)
-
-Vue.use(AmplifyPlugin, AmplifyModules)
-Vue.component(components)
