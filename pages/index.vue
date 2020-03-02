06331d3da352b4791d254c54b8b89357d2675484
Jakub Voráček <jakub.voracek@gmail.com>
2020-03-02 14:46:26 +0100

    add login test

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
     <div class="flex flex-wrap -mx-4">
+      <div>kkkk
+        <button @click="click({provider: 'Facebook'})">Open Facebook</button>
+        <button @click="click({provider: 'Google'})">Open Google</button>
+        <button @click="click()">Open Hosted UI</button>
+        <button @click="Auth.signOut()">Sign Out</button>
+      </div>
       <div
         v-for="n in 8"
         :key="n"
@@ -16,18 +22,22 @@
 
 <script>
 import VCard from "@/components/molecules/Card";
-import { Auth } from "aws-amplify";
 import { AmplifyEventBus } from "aws-amplify-vue";
+
+import Amplify, { Auth, Hub } from "aws-amplify";
+
 export default {
   data() {
     return {
-      signedIn: false
+      signedIn: false,
+      user: null,
+      customState: null
     };
   },
   components: { VCard },
   methods: {
-    click() {
-      return Auth.federatedSignIn({ provider: "Facebook" });
+    click(option) {
+      return Auth.federatedSignIn(option);
     },
     async findUser() {
       try {
