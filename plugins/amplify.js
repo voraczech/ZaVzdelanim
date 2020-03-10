cdd04035525253782bbe54f93cb1a67f89a27307
Jakub Voráček <jakub.voracek@gmail.com>
2020-03-10 18:57:18 +0100

    POC Amplify connect

--- plugins/amplify.js
+++ plugins/amplify.js
@@ -1,4 +1,8 @@
-import Amplify from 'aws-amplify'
+import Vue from "vue";
+import Amplify, * as AmplifyModules from 'aws-amplify'
+import { AmplifyPlugin } from 'aws-amplify-vue'
 import awsconfig from '@/aws-exports'
 
 Amplify.configure(awsconfig)
+
+Vue.use(AmplifyPlugin, AmplifyModules)
