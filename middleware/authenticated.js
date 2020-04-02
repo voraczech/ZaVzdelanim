674f22d0693c6a1f025904dd91d605eaf86922af
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-02 15:55:39 +0200

    refirect after login

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -3,13 +3,6 @@ import Auth from "@aws-amplify/auth";
 export default async function ({ store, redirect }) {
   // If the user is not authenticated
   if (!store.state.user) {
-    // Hub.listen("auth", ({ payload: { event } }) => {
-    //   if (event === `signIn`) {
-    //     // const user = await Auth.currentAuthenticatedUser();
-    //     // store.commit(`setUser`, user.attributes)
-    //   }
-    // });
-
     try {
       const user = await Auth.currentAuthenticatedUser();
       store.commit(`setUser`, user.attributes)
--- middleware/signin.js
+++ middleware/signin.js
@@ -1,9 +1,16 @@
-import { Auth, Hub } from "aws-amplify";
+import Auth from "@aws-amplify/auth";
+import { Hub } from "@aws-amplify/core";
 
 export default async function ({ store, redirect }) {
   if (store.state.user) {
     return redirect("/");
   }
+
+  Hub.listen("auth", ({ payload: { event } }) => {
+    if (event === `signIn`) {
+      return redirect("/");
+    }
+  });
 
   try {
     const user = await Auth.currentAuthenticatedUser();
