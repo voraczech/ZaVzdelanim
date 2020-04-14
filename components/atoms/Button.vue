86eb9f0cd0280dba4cf11120ef4c49309d3d51e6
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-14 19:15:19 +0200

    add button and link component

---
+++ components/atoms/Button.vue
@@ -0,0 +1,8 @@
+<template>
+  <button
+    class="rounded  px-4 py-2 shadow uppercase tracking-wider font-semibold hover:shadow-md transition-shadow duration-200 fill-current flex items-center justify-center bg-blue-300 text-blue-900"
+  >
+    <slot />
+  </button>
+</template>
+
---
+++ components/atoms/Link.vue
@@ -0,0 +1,29 @@
+<template>
+  <nuxt-link
+    :is="is"
+    :to="to"
+    :a="a"
+    :_target="is === `a` ? `blank` : ``"
+    class="text-blue-700"
+  >
+    <slot />
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: {
+    a: {
+      type: String
+    },
+    to: {
+      type: String
+    }
+  },
+  computed: {
+    is() {
+      return !!this.a ? `a` : `nuxt-link`;
+    }
+  }
+};
+</script>
