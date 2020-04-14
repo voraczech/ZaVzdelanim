e9cb87f9637df3e472a7b18284086cd54b508575
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-14 17:14:06 +0200

    add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': 'Inter, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+      },
+      inset: {
+        '4': '1rem'
+      }
+    },
+  },
   variants: {},
-  plugins: []
+  plugins: [
+  ]
 }
