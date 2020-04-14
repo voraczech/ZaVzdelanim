21a29114ec6965a55657e8f4864c46e54e36ac28
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-14 19:18:30 +0200

    add first volume of icons

---
+++ plugins/unicons.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Unicon from 'vue-unicons'
+import { uniTimesCircleMonochrome, uniCheckCircleMonochrome, uniPresentation, uniPodium, uniBuilding, uniCog, uniMapMarker, uniCalender } from 'vue-unicons/src/icons'
+
+Unicon.add([uniTimesCircleMonochrome, uniCheckCircleMonochrome, uniPresentation, uniPodium, uniBuilding, uniCog, uniMapMarker, uniCalender])
+Vue.use(Unicon)
