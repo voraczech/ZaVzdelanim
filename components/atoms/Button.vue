f7c9938740971a37b5dc66dd411883e8458e1cea
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-14 19:17:35 +0200

    style button not-to-be so much materialui

--- components/atoms/Button.vue
+++ components/atoms/Button.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="rounded  px-4 py-2 shadow uppercase tracking-wider font-semibold hover:shadow-md transition-shadow duration-200 fill-current flex items-center justify-center bg-blue-300 text-blue-900"
+    class="rounded  px-4 py-2 shadow tracking-wider font-semibold hover:shadow-md transition-shadow duration-200 fill-current flex items-center justify-center bg-blue-300 text-blue-900"
   >
     <slot />
   </button>
