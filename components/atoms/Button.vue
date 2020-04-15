9d6cb8e46901936fe9f528f4f5452acc09bdfed1
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-15 17:02:59 +0200

    style button

--- components/atoms/Button.vue
+++ components/atoms/Button.vue
@@ -1,8 +1,23 @@
 <template>
   <button
-    class="rounded  px-4 py-2 shadow tracking-wider font-semibold hover:shadow-md transition-shadow duration-200 fill-current flex items-center justify-center bg-blue-300 text-blue-900"
+    class="bg-white tracking-wide text-gray-800 font-bold fill-current rounded border-b-2 hover:bg-blue-500 hover:text-blue-100 hover:border-blue-600 shadow-md py-2 px-6 inline-flex items-center justify-center transition-all duration-75"
+    :class="[
+    (type === `default`) && `border-blue-500`,
+    (type === `cta`) && `border-purple-500 bg-purple-200 text-purple-800 hover:bg-purple-800 hover:text-purple-200 hover:border-purple-500`,
+    (type === `ctaActivated`) && `border-purple-500 bg-purple-800 text-purple-200 hover:bg-purple-200 hover:text-purple-800 hover:border-purple-500`,
+    ]"
   >
     <slot />
   </button>
 </template>
 
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "default"
+    }
+  }
+};
+</script>
