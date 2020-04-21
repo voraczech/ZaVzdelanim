f67122bd4b12c450800f4488f7792b15e15f202f
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-21 16:55:55 +0200

    style atoms

---
+++ components/atoms/Input.vue
@@ -0,0 +1,18 @@
+<template>
+  <input
+    ref="input"
+    :type="type"
+    :value="value"
+    class="px-4 py-3 bg-white border-b-2 border-gray-400 rounded transition duration-75 focus:border-blue-500 hover:shadow"
+    @input="$emit('input', $event.target.value)"
+  >
+</template>
+
+<script>
+export default {
+  props: {
+    value: String,
+    type: String
+  }
+};
+</script>
---
+++ components/atoms/TextButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button
+    class="tracking-wide font-semibold fill-current inline-flex items-center justify-center transition-all duration-75"
+    :class="[
+    (design === `default`) && `text-blue-600 hover:text-blue-800`,
+    (design === `alert`) && `text-red-600 hover:text-red-800`
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    design: {
+      type: String,
+      default: "default"
+    }
+  }
+};
+</script>
