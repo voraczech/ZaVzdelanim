78d0307e509b01866368ba469ba26c58438ecbc1
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-21 16:56:22 +0200

    show organization events

--- pages/organization/_organization/index.vue
+++ pages/organization/_organization/index.vue
@@ -7,41 +7,67 @@
           Chyba, to mě mrzí.
         </div>
       </div>
-      <div v-else-if="data.getOrganization">
-        <div class="flex justify-between items-center">
-          <h1 class="text-2xl font-bold bg-white shadow rounded-lg px-5 py-4">{{ data.getOrganization.name }}</h1>
-          <nuxt-link
-            :to="`/organization/${data.getOrganization.id}/edit`"
-            class="bg-white shadow rounded-lg px-5 py-4 w-1/2 my-15 w-6"
-            v-if="isOwner(data.getOrganization.owner)"
-          >
-            Upravit
-          </nuxt-link>
-          <nuxt-link
-            :to="`/organization/${data.getOrganization.id}/new-event`"
-            class="bg-white shadow rounded-lg px-5 py-4 w-1/2 my-15 w-6"
-            v-if="isOwner(data.getOrganization.owner)"
-          >
-            Přidat událost
-          </nuxt-link>
+      <div
+        v-else-if="data.getOrganization"
+        class="px-2"
+      >
+        <div class="flex -mx-2">
+          <div class="w-2/3 px-2">
+            <div class="rounded shadow-sm bg-white">
+              <img
+                src=""
+                alt=""
+              />
+              <div class="p-12">
+                <h1 class="text-2xl font-bold">{{data.getOrganization.name}}</h1>
+                <p
+                  class="mt-8"
+                  v-if="data.getOrganization.description"
+                >{{ data.getOrganization.description }}</p>
+              </div>
+            </div>
+          </div>
+          <div class="w-1/3 px-2">
+            <nuxt-link
+              :to="`/organization/${data.getOrganization.id}/new-event`"
+              v-if="isOwner(data.getOrganization.owner)"
+            >
+              <v-button class="mb-4 w-full">Přidat událost
+                <unicon
+                  name="plus"
+                  class="ml-3"
+                />
+              </v-button>
+            </nuxt-link>
+            <nuxt-link
+              :to="`/organization/${data.getOrganization.id}/edit`"
+              v-if="isOwner(data.getOrganization.owner)"
+            >
+              <v-button class="mb-4 w-full">Upravit</v-button>
+            </nuxt-link>
+          </div>
         </div>
-        <img
-          src=""
-          alt=""
-        />
-        <div class="bg-white shadow rounded-lg px-5 py-4 w-1/2 my-5">{{ data.getOrganization.description }}</div>
-
-        <h2 class="text-2xl font-bold bg-white shadow rounded-lg px-5 py-4 mt-8">Události</h2>
-        <div class="flex flex-wrap -mx-4">
-          <!-- <div
-            v-for="n in 8"
-            :key="n"
+        <h2 class="text-xl font-semibold mt-8">Události</h2>
+        <div
+          class="flex flex-wrap -mx-4"
+          v-if="data.getOrganization.host.items.length > 0"
+        >
+          <div
+            v-for="({event}, key) in data.getOrganization.host.items"
+            :key="key"
             class="p-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4"
           >
-            <nuxt-link to="event/1">
-              <VCard />
+            <nuxt-link
+              :to="`event/${event.id}`"
+              v-if="event !== null"
+            >
+              <VCard>
+                {{ event.title }}
+                {{ event.date }}
+                {{ event.speaking.items.length !== 0 ? event.speaking.items : `` }}
+              </VCard>
             </nuxt-link>
-          </div> -->
+          </div>
         </div>
       </div>
     </template>
@@ -49,15 +75,34 @@
 </template>
 
 <script>
-import VCard from "@/components/molecules/Card";
 import { mapState } from "vuex";
-import { components } from "aws-amplify-vue";
 
-const getOrg = `query getOrganization($id: ID!) {
+import VButton from "@/components/atoms/Button";
+import VCard from "@/components/molecules/Card";
+
+const getOrg = `
+query getOrganization($id: ID!) {
   getOrganization(id: $id){
-      id
-      name
-      owner
+    id
+    name
+    owner
+    admins{items{userID}}
+    host{
+      items{
+        event{
+          id
+          title
+          date
+          speaking {
+            items {
+              speaker{
+                name
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
 `;
@@ -68,7 +113,7 @@ const editOrg = `mutation updateOrg{
 `;
 
 export default {
-  components: { VCard, ...components },
+  components: { VButton, VCard },
 
   async asyncData({ params }) {
     const orgId = params.organization;
