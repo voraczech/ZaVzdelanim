5a9c3538657c133775c0487659fdf473341077dd
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-21 20:08:34 +0200

    unite input styling

--- components/atoms/Input.vue
+++ components/atoms/Input.vue
@@ -3,7 +3,6 @@
     ref="input"
     :type="type"
     :value="value"
-    class="px-4 py-3 bg-white border-b-2 border-gray-400 rounded transition duration-75 focus:border-blue-500 hover:shadow"
     @input="$emit('input', $event.target.value)"
   >
 </template>
