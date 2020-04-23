3d5341ab22682154b9d100d742fdd43fa958bd69
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-23 21:59:54 +0200

    card is link

--- components/atoms/Card.vue
+++ components/atoms/Card.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="bg-white rounded shadow-lg hover:shadow-xl flex flex-col overflow-hidden duration-200">
+  <nuxt-link
+    :to="to"
+    class="bg-white rounded shadow-lg hover:shadow-xl flex flex-col overflow-hidden duration-200"
+  >
     <div class="m-4">
       <slot />
     </div>
-  </div>
+  </nuxt-link>
 </template>
+
+<script>
+export default {
+  props: {
+    to: {
+      type: String,
+      default: ""
+    }
+  }
+};
+</script>
