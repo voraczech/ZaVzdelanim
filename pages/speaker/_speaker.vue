b91b3abf28ba0dcab5b628130072b7954042ee69
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-23 22:00:57 +0200

    show speaker photo if exists

--- pages/speaker/_speaker.vue
+++ pages/speaker/_speaker.vue
@@ -7,8 +7,22 @@
           Chyba, to mě mrzí.
         </div>
       </div>
-      <v-detail v-else-if="data.getSpeaker">
-        <template slot="title">{{ data.getSpeaker.name }}</template>
+      <v-detail
+        v-else-if="data.getSpeaker"
+        :events="data.getSpeaker.speaking.items"
+      >
+        <template slot="title">
+          <div class="flex items-center">
+            <v-image
+              class="w-24 mr-8"
+              v-if="data.getSpeaker.avatar"
+              :path="data.getSpeaker.avatar"
+            />
+            <span>
+              {{ data.getSpeaker.name }}
+            </span>
+          </div>
+        </template>
         <template>{{ data.getSpeaker.bio }}</template>
       </v-detail>
     </template>
@@ -16,13 +30,39 @@
 </template>
 
 <script>
+import VImage from "@/components/atoms/Image";
 import VDetail from "@/components/templates/Detail";
 
 import { mapState } from "vuex";
-import { getSpeaker } from "../../src/graphql/queries";
+
+const getSpeaker = `
+query getSpeaker($id: ID!){
+  getSpeaker(id: $id){
+    name
+    bio
+    avatar
+    speaking{
+      items{
+        event{
+          title
+          date
+          image
+          speaking{
+            items{
+              speaker{
+                name
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+`;
 
 export default {
-  components: { VDetail },
+  components: { VImage, VDetail },
 
   async asyncData({ params }) {
     const speakerId = params.speaker;
