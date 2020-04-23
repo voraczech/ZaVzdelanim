321855a2001891c268cc6e1fb1c4edb4e1cc0f5d
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-23 17:13:29 +0200

    make detail component

---
+++ components/templates/Detail.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="px-2">
+    <div class="flex -mx-2">
+      <div class="w-2/3 px-2">
+        <div class="rounded shadow-sm bg-white">
+          <div class="p-12">
+            <h1 class="text-2xl font-bold mb-8">
+              <slot name="title" />
+            </h1>
+            <slot />
+          </div>
+        </div>
+      </div>
+      <div
+        class="w-1/3 px-2"
+        v-if="!!$slots.box || !!$slots.aboveBox"
+      >
+        <slot name="aboveBox" />
+        <div class="rounded p-8 shadow-sm bg-white sticky top-4">
+          <slot name="box" />
+        </div>
+      </div>
+    </div>
+    <div v-if="events.length > 0">
+      <h2 class="text-xl font-semibold mt-8">
+        <slot name="eventsTitle">Události</slot>
+      </h2>
+      <div class="flex flex-wrap -mx-4">
+        <div
+          v-for="(event, key) in events"
+          :key="key"
+          class="p-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4"
+        >
+          <nuxt-link :to="`/event/${event.id}`">
+            <v-event-card />
+          </nuxt-link>
+        </div>
+        <nuxt-link
+          to="/events"
+          class="ml-auto"
+        >
+          <v-text-button>Všechny události</v-text-button>
+        </nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import VTextButton from "@/components/atoms/TextButton";
+import VEventCard from "@/components/organisms/EventCard";
+
+export default {
+  components: { VTextButton, VEventCard },
+  props: {
+    events: {
+      type: [Array, Object],
+      default: () => []
+    }
+  }
+};
+</script>
