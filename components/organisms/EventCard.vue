c01681fab44720e39c8c575378cfef1c608da3cf
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-23 19:14:02 +0200

    style eventCard

--- components/organisms/EventCard.vue
+++ components/organisms/EventCard.vue
@@ -1,36 +1,37 @@
 <template>
-  <v-card>
-      <img
-        src="/icons/user.png"
-        class="w-full"
-      />
-      <h3 class="font-bold text-sm">
-        <slot name="title" />
-      </h3>
-      <div class="text-gray-600 text-xs">
-        <div class="mt-3 flex">
-          <div class="px-1 mr-3 flex items-center justify-center">
-            <img
-              src="/icons/date.png"
-              alt=""
-              width="16"
-              height="16"
-            />
-          </div>
-          <span>{{  }}</span>
-        </div>
-        <div class="mt-3 flex">
-          <div class="mr-3 rounded-full bg-gray-300 p-1 w-6 h-6 flex items-center justify-center">
-            <img
-              src="/icons/user.png"
-              alt=""
-              height="16"
-              width="16"
-            />
-          </div>
-          <span>Pavlína Louženská</span>
+  <v-card
+    :to="`/event/${event.id}`"
+    v-if="event"
+  >
+    <img
+      :src="url"
+      class="w-full"
+    />
+    <h3 class="font-bold text-sm">{{event.title}}</h3>
+    <div class="text-gray-600 text-xs fill-current leading-6">
+      <div class="mt-3 inline-flex">
+        <unicon
+          name="calender"
+          class="mr-3"
+        />
+        <span>{{ $moment(event.date).format("llll") }}</span>
+      </div>
+      <div
+        class="mt-3 flex"
+        v-if="event.speaking.items.length > 0"
+      >
+        <unicon
+          name="podium"
+          class="mr-3"
+        />
+        <div>
+          <span
+            v-for="({speaker}, key) in event.speaking.items"
+            :key="key"
+          >{{ speaker.name + (event.speaking.items.length === key+1 ? `` : `, `)}}</span>
         </div>
       </div>
+    </div>
   </v-card>
 </template>
 
@@ -39,6 +40,34 @@ import VCard from "@/components/atoms/Card";
 
 export default {
   components: { VCard },
-
+  props: {
+    to: {
+      type: String,
+      default: ""
+    },
+    event: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  data() {
+    return {
+      url: null
+    };
+  },
+  mounted() {
+    this.getImage();
+  },
+  methods: {
+    getImage() {
+      if (this.event.image) {
+        this.$Amplify.Storage.get(this.event.image, this.options)
+          .then(url => {
+            this.url = url;
+          })
+          .catch(e => this.setError(e));
+      }
+    }
+  }
 };
 </script>
