e27ea8c4b5cc2afff46c364d8ee3b775bc08eb5f
Jakub Voráček <jakub.voracek@gmail.com>
2020-04-25 18:44:43 +0200

    new icons

--- plugins/unicons.js
+++ plugins/unicons.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Unicon from 'vue-unicons'
-import { uniTimesCircleMonochrome, uniCheckCircleMonochrome, uniPresentation, uniPodium, uniBuilding, uniCog, uniMapMarker, uniCalender, uniPlus, uniUserPlus, uniSlidersVAlt, uniSad, uniMinusCircle, uniPlusCircle } from 'vue-unicons/src/icons'
+import { uniTimesCircleMonochrome, uniCheckCircleMonochrome, uniPresentation, uniPodium, uniBuilding, uniCog, uniMapMarker, uniCalender, uniPlus, uniUserPlus, uniSlidersVAlt, uniSad, uniMinusCircle, uniPlusCircle, uniUsersAlt, uniLink } from 'vue-unicons/src/icons'
 
-Unicon.add([uniTimesCircleMonochrome, uniCheckCircleMonochrome, uniPresentation, uniPodium, uniBuilding, uniCog, uniMapMarker, uniCalender, uniPlus, uniUserPlus, uniSlidersVAlt, uniSad, uniMinusCircle, uniPlusCircle])
+Unicon.add([uniTimesCircleMonochrome, uniCheckCircleMonochrome, uniPresentation, uniPodium, uniBuilding, uniCog, uniMapMarker, uniCalender, uniPlus, uniUserPlus, uniSlidersVAlt, uniSad, uniMinusCircle, uniPlusCircle, uniUsersAlt, uniLink])
 Vue.use(Unicon)
